5322d18b3f332d82e97db551871247f062ebf050
Jakub Jagielka <jjagielka@gmail.com>
2022-03-21 13:39:38 +0100

    sortable - container removed

--- src/lib/UI/Sortable.svelte
+++ src/lib/UI/Sortable.svelte
@@ -1,24 +1,32 @@
+<script context="module">
+    // your script goes here
+</script>
+
 <script>
-    import Sortable from "sortablejs";
-    import { createEventDispatcher } from "svelte";
+    import Sortable from 'sortablejs';
+    import { createEventDispatcher } from 'svelte';
 
     const dispatch = createEventDispatcher();
 
     function notify(el) {
         const val = [...el.children].map((i) => i.dataset.id);
-        dispatch("change", val);
+        dispatch('change', val);
     }
 
     export let options = {};
     export let items = [];
 
     function create(node) {
-        const sortable = Sortable.create(node, {
+        const sortable = Sortable.create(node.parentNode, {
             ...options,
             onUpdate: (ev) => notify(ev.to),
             onAdd: (ev) => notify(ev.to),
             onRemove: (ev) => notify(ev.from),
         });
+
+        // Temporary node removal
+        node.parentNode.removeChild(node);
+
         return {
             update() {},
             onDestroy() {
@@ -28,8 +36,7 @@
     }
 </script>
 
-<div use:create>
-    {#each items as item (item)}
-        <slot {item} />
-    {/each}
-</div>
+<div use:create><!-- Temporary node only for the initialization. --></div>
+{#each items as item (item)}
+    <slot {item} />
+{/each}
