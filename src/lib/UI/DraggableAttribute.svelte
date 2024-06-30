a2344729fb85674b02012af46f1ac1d80a3011e5
Alex van der Valk <21194068+alexvdvalk@users.noreply.github.com>
2024-06-30 20:41:22 +0100

    remove import from svelte/internal

--- src/lib/UI/DraggableAttribute.svelte
+++ src/lib/UI/DraggableAttribute.svelte
@@ -1,37 +1,43 @@
 <script>
-    import Draggable from "./Draggable.svelte";
-    import FilterBox from "./FilterBox.svelte";
-    import { is_empty } from "svelte/internal";
+  import Draggable from "./Draggable.svelte";
+  import FilterBox from "./FilterBox.svelte";
 
-    export let valueFilter;
-    export let name;
-    export let attrValues;
-    export let menuLimit;
-    export let updateValuesInFilter;
+  export let valueFilter;
+  export let name;
+  export let attrValues;
+  export let menuLimit;
+  export let updateValuesInFilter;
 
-    let open = false;
+  let open = false;
 
-    const toggleOpen = () => (open = !open);
+  $: is_empty = Object.keys(valueFilter).length === 0;
+
+  const toggleOpen = () => (open = !open);
 </script>
 
 <li data-id={name}>
-    <span class={`pvtAttr ${is_empty(valueFilter) ? "" : "pvtFilteredAttribute"}`}>
-        {name}
-        <span class="pvtTriangle" on:click={toggleOpen} on:keypress={toggleOpen}>
-            {" "}
-            ▾
-        </span>
+  <span class={`pvtAttr ${is_empty ? "" : "pvtFilteredAttribute"}`}>
+    {name}
+    <span class="pvtTriangle" on:click={toggleOpen} on:keypress={toggleOpen}>
+      {" "}
+      ▾
     </span>
+  </span>
 
-    {#if open}
-        <Draggable handle=".pvtDragHandle" close=".pvtCloseX" on:click={toggleOpen} on:close={toggleOpen}>
-            <FilterBox
-                {name}
-                {valueFilter}
-                values={attrValues}
-                {menuLimit}
-                on:change={(ev) => updateValuesInFilter(name, ev.detail)}
-            />
-        </Draggable>
-    {/if}
+  {#if open}
+    <Draggable
+      handle=".pvtDragHandle"
+      close=".pvtCloseX"
+      on:click={toggleOpen}
+      on:close={toggleOpen}
+    >
+      <FilterBox
+        {name}
+        {valueFilter}
+        values={attrValues}
+        {menuLimit}
+        on:change={(ev) => updateValuesInFilter(name, ev.detail)}
+      />
+    </Draggable>
+  {/if}
 </li>
